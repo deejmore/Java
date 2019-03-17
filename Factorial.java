c5734bafcf6234823e03a43d33e9dab3fbef786f
deejmore <deejmore@me.com>
2019-03-17 17:58:34 +1100

    modify this file

--- Factorial.java
+++ Factorial.java
@@ -1,18 +1,39 @@
+import java.util.Scanner;
+
 public class Factorial {
+
+	private static String space = " ";
+
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Factorial factorial = new Factorial();
-		System.out.println(factorial.getRecursiveFactorial(6));
-		System.out.println(factorial.getIterativeFactorial(6));
+		Scanner in = new Scanner(System.in);
+		int num = 0;
+		do
+		{
+			num = in.nextInt();
+		System.out.println(factorial.getRecursiveFactorial(num));
+		System.out.println(factorial.getIterativeFactorial(num));
+		}
+		while (num != 0);
 	}
-	
+
 	public int getRecursiveFactorial(int n) {
-		if (n < 0) return -1;
-		else if (n < 2) return 1;
-		else return (n * getRecursiveFactorial(n-1));		
+		System.out.print(n);
+		System.out.print(space);
+		if (n < 0)
+			return -1;
+		else if (n < 2)
+			return 1;
+		else
+			return (n * getRecursiveFactorial(n - 1));
 	}
-	
+
 	public int getIterativeFactorial(int n) {
-		if (n < 0) return -1;
+		System.out.print(n);
+		System.out.print(space);
+		if (n < 0)
+			return -1;
 		int fact = 1;
 		for (int i = 1; i <= n; i++)
 			fact *= i;
